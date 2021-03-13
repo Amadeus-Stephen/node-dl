b07827ca21967b687d459ade090246e601c8ac66
Robel Schwarz <itsrobel.schwarz@gmail.com>
2021-03-12 20:31:01 -0500

    works now lol

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-const express = require("express")
-const axios = require("axios")
-const {JSDOM} = require("jsdom")
-const { linkSync } = require("fs")
-const readline = require("readline").createInterface({
-	input: process.stdin,
-	output: process.stdout
-})
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio')
+const prompt = require('prompt-sync')();
+const list = require('cli-list-select');
+const youtubedl = require('youtube-dl')
 
-
-const app =  express()
-const port = 3000
-
-
-
-const getSearchData = async () => {
-	try { 
-		readline.question("what would you like to search? " , raw_query => {
-			const {data} = await axios.get(`https://www.youtube.com/results?search_query=${raw_query}`)
-			const dom = new JSDOM(data)
-			const {document} = dom.window
-			let link_array = document.querySelectorAll("a.ytd-video-renderer#video-title")
-			console.log(link_array)
-			readline.close()
-		})
-	}
-	catch (error) {
-		throw error;
-	}
+const getPageHTML = async (raw_query) => {
+    const pageUrl = `https://www.youtube.com/results?search_query=${raw_query}`
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(pageUrl);
+    const pageHTML = await page.evaluate('new XMLSerializer().serializeToString(document.doctype) + document.documentElement.outerHTML');
+    await browser.close();
+    return pageHTML;
 }
 
-app.listen(port)
+const borrowTheMp3 = async() => {
+    const raw_query = prompt('what do you want to search?: ');
+    const html = await getPageHTML(raw_query)
+    const $ = cheerio.load(html)
+    console.clear()
+    const video_list = $('a.ytd-video-renderer[id="video-title"]')
+    let titile_list  = []
+    video_list.each((i , video) => {
+        titile_list.push(video.attribs.title)
+    })
+    const user_selection = await list(titile_list , {singleCheck:true})
+    const video_url = video_list[user_selection.index].attribs.href
+    const video = youtubedl.exec(`http://www.youtube.com/${video_url}`, [`-o ${raw_query}.mp3`, '-x', '--audio-format', "mp3"], {} , (err , output) => {
+        if (err) throw err
+        console.log(output.join('\n'))
+        process.exit()
+    })
+}
+borrowTheMp3()
