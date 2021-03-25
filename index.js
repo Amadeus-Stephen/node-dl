ce6c7bee35205c0562e271062d198a1895c0d74d
Robel Schwarz <itsrobel.schwarz@gmail.com>
2021-03-25 01:51:34 +0000

    set to music folder

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const cheerio = require('cheerio')
 const prompt = require('prompt-sync')();
 const list = require('cli-list-select');
 const youtubedl = require('youtube-dl')
+const fs = require('fs')
+const path = require('path')
 
+
+const MUSIC_FOLDER_PATH = "/home/amadeus/Music/"
 const getPageHTML = async (raw_query) => {
     const pageUrl = `https://www.youtube.com/results?search_query=${raw_query}`
     const browser = await puppeteer.launch();
@@ -16,6 +20,8 @@ const getPageHTML = async (raw_query) => {
 
 const borrowTheMp3 = async() => {
     const raw_query = prompt('what do you want to search?: ');
+    const currentPath = path.join(__dirname, ` ${raw_query}.mp3`)
+    const destinationPath = path.join(MUSIC_FOLDER_PATH , `${raw_query}.mp3`)
     const html = await getPageHTML(raw_query)
     const $ = cheerio.load(html)
     console.clear()
@@ -29,7 +35,13 @@ const borrowTheMp3 = async() => {
     const video = youtubedl.exec(`http://www.youtube.com/${video_url}`, [`-o ${raw_query}.mp3`, '-x', '--audio-format', "mp3"], {} , (err , output) => {
         if (err) throw err
         console.log(output.join('\n'))
-        process.exit()
+        fs.rename(currentPath,destinationPath, (err2) => {
+            if (err2) throw err2
+            console.log('Save to Music Dir')
+            process.exit()
+        })
     })
+ 
+    
 }
 borrowTheMp3()
